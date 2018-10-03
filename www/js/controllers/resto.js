a03ea7eb2087d2e727a60a3864a10b7a594c9a25
Angéline <contact@angeline-toussaint.fr>
2018-10-03 19:37:30 +0200

    update addresses

--- www/js/controllers/resto.js
+++ www/js/controllers/resto.js
@@ -11,42 +11,42 @@ app.controller('RestoCtrl', ['$scope', function ($scope) {
             category : 'Burger',
             name : 'Jab\'s Burger',
             price : '€',
-            address : '1 rue Francoise Giroud, 44200, Nantes',
+            address : '1 rue Francoise Giroud, 44200 Nantes',
             picture : 'jabsBurger'
         },
         {
             category : 'Burger',
             name : 'Le Melting Potes - Burger',
             price : '€',
-            address : '26 bd de la Prairie au Duc | Ile de Nantes, 44200, Nantes',
+            address : '26 bd de la Prairie au Duc, 44200 Nantes',
             picture : 'leMeltingPotes'
         },
         {
             category : 'Sandwicherie',
             name : 'Waffle Factory',
             price : '€',
-            address : '6 Rue du Docteur Zamenhof | Centre Commercial Beaulieu, 44200, Nantes',
+            address : 'Centre Commercial Beaulieu, 44200 Nantes',
             picture : 'waffleFactory'
         },
         {
             category : 'Sandwicherie',
             name : 'O\'Tacos',
             price : '€',
-            address : 'Centre Commercial Beaulieu, 44200, Nantes',
+            address : 'Centre Commercial Beaulieu, 44200 Nantes',
             picture : 'oTacos'
         },
         {
             category : 'Sandwicherie',
             name : 'Brioche Dorée',
             price : '€',
-            address : 'Centre Commercial Beaulieu, 44200, Nantes',
+            address : 'Centre Commercial Beaulieu, 44200 Nantes',
             picture : 'briocheDoree'
         },
         {
             category : 'Grande Surface',
             name : 'Carrefour',
             price : '€',
-            address : 'Centre Commercial Beaulieu, 44200, Nantes',
+            address : 'Centre Commercial Beaulieu, 44200 Nantes',
             picture : 'carrefour'
         },
         {
@@ -74,63 +74,63 @@ app.controller('RestoCtrl', ['$scope', function ($scope) {
             category : 'Sandwicherie',
             name : 'Subway',
             price : '€/€€',
-            address : 'Centre Commercial Carrefour Beaulieu, 44200, Nantes',
+            address : 'Centre Commercial Beaulieu, 44200 Nantes',
             picture : 'subway'
         },
         {
             category : 'Burger',
             name : '231 East Street Nantes',
             price : '€€',
-            address : 'Boulevard du Général de Gaulle | Centre Commercial Beaulieu, 44200, Nantes',
+            address : 'Centre Commercial Beaulieu, 44200 Nantes',
             picture : '231EastStreet'
         },
         {
             category : 'Restaurant',
             name : 'Chromosome Restaurant',
             price : '€€',
-            address : '6 Promenade Europa, 44200, Nantes',
+            address : '6 Promenade Europa, 44200 Nantes',
             picture : 'chromosomeRestaurant'
         },
         {
             category : 'Pizzeria',
             name : 'La Villa Medicis',
             price : '€€/€€€',
-            address : 'Rue Gaetan Rondeau | Centre Commercial Beaulieu, 44200, Nantes',
+            address : 'Centre Commercial Beaulieu, 44200 Nantes',
             picture : 'laVillaMedicis'
         },
         {
             category: 'Pizzeria',
             name: '180G Pizza',
             price: '€€/€€€',
-            address: 'Place Francois II | 6c, 44200, Nantes',
+            address: 'Place Francois II, 44200 Nantes',
             picture : '180GPizza'
         },
         {
             category : 'Pizzeria',
             name : 'Pizza Tradition',
             price : '€€/€€€',
-            address : '10 boulevard Adolphe Billault | Boulevard Victor Hugo, 44200, Nantes',
+            address : '10 boulevard Adolphe Billault, 44200 Nantes',
             picture : 'pizzaTradition'
         },
         {
             category : 'Pizzeria',
             name : 'Le Comptoir Sale & Pepe',
             price : '€€/€€€',
-            address : '21 Rue Foure, 44000, Nantes',
+            address : '21 Rue Foure, 44000 Nantes',
             picture : 'leComptoirSalePepe'
         },
         {
             category : 'Restaurant',
             name : 'Les Fonderies',
             price : '€€/€€€',
-            address : '25 boulevard Vincent Gache, 44200, Nantes',
+            address : '25 boulevard Vincent Gache, 44200 Nantes',
             picture : 'lesFonderies'
         },
         {
             category : 'Restaurant',
             name : 'La Passerelle de Marcel',
             price : '€€/€€€',
-            address : '7 allée Jacques Berque | Quai Malakoff, 44000, Nantes',
+            address : '7 allée Jacques Berque, 44000 Nantes',
             picture : 'laPasserelleDeMarcel'
         }
     ]
